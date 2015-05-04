431a27be188f3fe5c17d97311e614ec9c56af09b
Tk01 <lesage.tim@telenet.be>
2015-05-04 06:03:48 +0200

    extra

---
+++ project.py
@@ -0,0 +1,25 @@
+import cv2
+import cv2.cv as cv
+import numpy as np
+import Image
+def getModelData():
+    return
+def reallign(data):
+    return
+def generateModel(data):
+    return    
+def getTestData():
+    return
+def fit(data,model):
+    return
+    return
+if __name__ == '__main__':
+    data = getModelData()
+    reallignedData = reallign(data)
+    pcaData = PCA(reallignedData)
+    model = generateModel(pcaData)
+    testData = getTestData()
+    result = fit(testData,model)
+    
+        
+        
