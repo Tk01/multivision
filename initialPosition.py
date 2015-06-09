c094a41caaa414c6907dd9b2d542416819e73978
BKevin <kevin_boets@hotmail.com>
2015-06-09 13:44:43 +0200

    initialize voor 4 bovenste tanden
    heb het getest(zie allResultss.jpg) en werkt redelijk,
    Misschien nog wat aanpassingen aan parameters nodig aangezien dit nogal
    snel snel was omgevormd.

--- initialPosition.py
+++ initialPosition.py
@@ -67,6 +67,14 @@ def pca(X, nb_components=0):
         v = np.dot(XminMean,eigenvectorsTemp[:,i])
         eigenvectors[:,i] = v/np.linalg.norm(v)
     
+    ii = np.hstack( (mean.reshape(size2,size1),
+                                 normalize(eigenvectors[:,0].reshape(size2,size1)),
+                                 normalize(eigenvectors[:,1].reshape(size2,size1)),
+                                 normalize(eigenvectors[:,2].reshape(size2,size1)))
+                               ).astype(np.uint8)
+    imm = cv2.resize(ii,(1500,400), interpolation=cv2.INTER_NEAREST)
+    cv2.imshow('pca',imm)
+    cv2.waitKey(0) 
     
     return [eigenvalues, eigenvectors, mean]
     
@@ -82,19 +90,60 @@ def makeData(resize1 = 500, resize2 = 500):
     results = np.zeros((14,resize2,resize1))
     
     smallImages = np.zeros((14,resize1 * resize2))
+    
+    listLeftTooth = [
+    [(1305,743), (1415,1015)],
+[(1307,626),(1403,965)],
+[(1339,660),(1463,982)],
+[(1322,679),(1420,1006)],
+[(1345,733),(1478,984)],
+[(1324,653),(1456, 930)],
+[(1320,653),(1424,972)],
+[(1357,588),(1478,881)],
+[(1358,715),(1465,1053)],
+[(1305,513),(1404,870)],
+[(1251,647),(1374,967)],
+[(1346,742),(1450,985)],
+[(1322,532),(1430,846)],
+[(1283,715),(1390,1008)] ]
+    
+    listTopFourTeeth = [
+    [(1298,752),(1730,1017)],
+[(1311,650),(1704,1004)],
+[(1335,688),(1762,998)],
+[(1318,684),(1697,1028)],
+[(1346,728),(1758,993)],
+[(1331,627),(1740,950)],
+[(1318,660),(1707,993)],
+[(1365,647),(1710,902)],
+[(1365,749),(1737,1063)],
+[(1307,519),(1669,876)],
+[(1249,658),(1669,980)],
+[(1342,753),(1692,998)],
+[(1309,547),(1707,859)],
+[(1281,709),(1680,1039)] ]
+
     for graphNumber in range(1,15):
         img = visualize.readRadiograph(graphNumber)
         
-        [(x1,y1),(x2,y2)] = estimateClick.askForEstimate(img)
+        #[(x1,y1),(x2,y2)] = estimateClick.askForEstimate(img)
+        [(x1,y1),(x2,y2)] = listTopFourTeeth[graphNumber-1]
         cutImage = img[y1:y2,x1:x2]
         result = cv2.resize(cutImage,(resize1,resize2), interpolation=cv2.INTER_NEAREST)
         results[graphNumber-1,:,:] = result
         
         smallImages[graphNumber - 1] = result.flatten()
     
+    allImages = np.zeros((resize2 * 2, resize1*8))
     for i in range(0,14):
-        cv2.imshow('img' + str(i) ,np.uint8(results[i,:,:]))
-        cv2.waitKey(0)
+        row = int(round(i / 8))
+        col = i % 8
+        allImages[resize2*row:resize2*(row+1),resize1*col:resize1*(col+1)] = results[i,:,:]
+        #cv2.imshow('img' + str(i) ,np.uint8(results[i,:,:]))
+        #cv2.waitKey(0)
+    reAll = cv2.resize(allImages,(1500,750), interpolation=cv2.INTER_NEAREST)
+    cv2.imshow('all initialization images' ,np.uint8(reAll))
+    cv2.waitKey(0)
     
     return smallImages   
        
@@ -104,18 +153,19 @@ def findPosition(mean, eigenvectors, image, size1, size2):
     
     best = 100000000
     bestPos = (-1,-1)
+    bestIm = np.zeros((500,400))
     #try different sizes
-    for size11 in range(18,23,2):
+    for size11 in range(16,21,2):
         for size22 in range(11,21,3):
             s1 = int(size1 * size11 / 20)
             s2 = int(size2 * size22 / 20)
             
             
-            a1 = int(m / 2 + m / 10)
+            a1 = int(m / 2 - m / 10)
             a2 = int(m / 2 + m / 4)
             aStep = 40
-            b1 = int(n / 2 - n / 8)
-            b2 = int(n / 2)
+            b1 = int(n / 2 - n / 16)
+            b2 = int(n / 2 + n / 16)
             bStep = 40
             
             #loop over all possible starting positions
@@ -137,16 +187,17 @@ def findPosition(mean, eigenvectors, image, size1, size2):
                     reX= reconstruct(eigenvectors, Y, mean)
                     
                     if np.linalg.norm(reX-X) < best:
-                        cv2.imshow(str(size11)+ ' - ' + str(size22) + '  /  ' + str(i) + ' - ' + str(j) ,reCut)
-                        cv2.waitKey(0)
+                        #cv2.imshow(str(size11)+ ' - ' + str(size22) + '  /  ' + str(i) + ' - ' + str(j) ,reCut)
+                        #cv2.waitKey(0)
                         best = np.linalg.norm(reX-X)
+                        bestIm = reCut
                         print best
                         sys.stdout.flush()
                         
                         bestPos =  (i,j)
             print str(s1) + " - " + str(s2)
             sys.stdout.flush()                
-    return bestPos
+    return (bestPos,bestIm)
 
 def testMatch(mean, vectors):
     
@@ -171,22 +222,27 @@ def testMatch(mean, vectors):
 
 
 if __name__ == '__main__':
-    size1 = 100
+    size1 = 500
     size2 = 400
     data = makeData(size1,size2)
     [eigenvalues, eigenvectors, mean] = pca(data)
-    cv2.imshow('img',np.hstack( (mean.reshape(size2,size1),
-                                 normalize(eigenvectors[:,0].reshape(size2,size1)),
-                                 normalize(eigenvectors[:,1].reshape(size2,size1)),
-                                 normalize(eigenvectors[:,2].reshape(size2,size1)))
-                               ).astype(np.uint8))
-    cv2.waitKey(0) 
+
     
     #testMatch(mean,eigenvectors)
     
+    allImages = np.zeros((size2 * 3, size1*5))
     
     for graphNumber in range(1,15):
          img = visualize.readRadiograph(graphNumber)
-         (a,b) = findPosition(mean, eigenvectors, img, size1, size2)
+         (a,b),im = findPosition(mean, eigenvectors, img, size1, size2)
          print str(a) + " - " + str(b)
          sys.stdout.flush()
+         i = graphNumber - 1
+         row = int(round(i / 5))
+         col = i % 5
+         allImages[size2*row:size2*(row+1),size1*col:size1*(col+1)] = im
+    
+    cv2.imwrite('allResultss.jpg',np.uint8(allImages))
+    
+    cv2.imshow('all results of findPosition' ,np.uint8(allImages))
+    cv2.waitKey(0)
